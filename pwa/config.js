a681d94f99160c00664d6474df76db9a5cd0baa5
GIGA山 <basstar.1102@gmail.com>
2026-08-13 08:38:29 +0900

    Set gasWebAppUrl in config.js

--- pwa/config.js
+++ pwa/config.js
@@ -3,5 +3,5 @@
  * デプロイ後に /exec で終わるURLを設定してください。
  */
 window.OPP_CONFIG = Object.freeze({
-  gasWebAppUrl: ''
+  gasWebAppUrl: 'https://script.google.com/macros/s/AKfycbzuFIui3SSuYyVO2LYhEOUMxXdPkSj_ynDxAbLeRvcZlSCQ0vZn7YVgUD2szkRtQQR_/exec'
 });
